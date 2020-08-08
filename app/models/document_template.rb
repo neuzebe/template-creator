f3f1356dbb9a44bf40e0bb0807bbf8c92b026678
Noel Euzebe <noel.euzebe@gmail.com>
2020-08-08 02:34:03 -0400

    add a todo for optional validation later

--- app/models/document_template.rb
+++ app/models/document_template.rb
@@ -5,4 +5,6 @@ class DocumentTemplate < ApplicationRecord
 
   validates :name   , presence: true
   validates :content, presence: true
+
+  # TODO maybe worthwhile to validate that no two questions on the same document have the same tag
 end
