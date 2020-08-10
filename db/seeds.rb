0e4ca170be1e2f82bedbce5bea3948838a53412c
Noel Euzebe <noel.euzebe@gmail.com>
2020-08-09 22:00:03 -0400

    comment out seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-@template  = FactoryBot.create(:document_template, content: 'Document Content. This will serve the purpose of {{reason}}. To be submitted to {{destination}}')
-
-@question  = FactoryBot.create(:question, tag: 'reason')
-@question.document_template_questions.create document_template_id: @template.id
-
-@question2 = FactoryBot.create(:question, tag: 'destination')
-@question2.document_template_questions.create document_template_id: @template.id
-
-@document  = FactoryBot.create(:document, document_template_id: @template.id)
+# @template  = FactoryBot.create(:document_template, content: 'Document Content. This will serve the purpose of {{reason}}. To be submitted to {{destination}}')
+#
+# @question  = FactoryBot.create(:question, tag: 'reason')
+# @question.document_template_questions.create document_template_id: @template.id
+#
+# @question2 = FactoryBot.create(:question, tag: 'destination')
+# @question2.document_template_questions.create document_template_id: @template.id
+#
+# @document  = FactoryBot.create(:document, document_template_id: @template.id)
